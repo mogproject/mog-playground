53f5ede42b1831fdec5931bcde87a3100b986dd9
Yosuke Mizutani <mogproj@gmail.com>
2017-01-15 22:57:57 -0500

    added more initial states

--- src/main/scala/com/mogproject/mogami/playground/view/Renderer.scala
+++ src/main/scala/com/mogproject/mogami/playground/view/Renderer.scala
@@ -178,11 +178,26 @@ case class Renderer(elem: Element, layout: Layout) {
   }
 
   object EditReset {
-    private[this] val states = Seq(State.HIRATE, State.MATING_BLACK, State.MATING_WHITE)
+    private[this] val states = Seq(
+      State.HIRATE, State.MATING_BLACK, State.MATING_WHITE,
+      State.HANDICAP_LANCE, State.HANDICAP_BISHOP, State.HANDICAP_ROOK, State.HANDICAP_ROOK_LANCE,
+      State.HANDICAP_2_PIECE, State.HANDICAP_3_PIECE, State.HANDICAP_4_PIECE, State.HANDICAP_5_PIECE,
+      State.HANDICAP_6_PIECE, State.HANDICAP_8_PIECE, State.HANDICAP_10_PIECE,
+      State.HANDICAP_THREE_PAWNS, State.HANDICAP_NAKED_KING
+    )
 
     private[this] val labels: Map[Language, Seq[String]] = Map(
-      Japanese -> Seq("平手", "詰将棋 (先手)", "詰将棋 (後手)"),
-      English -> Seq("Even", "Mating (Black)", "Mating (White)")
+      Japanese -> Seq(
+        "平手", "詰将棋 (先手)", "詰将棋 (後手)",
+        "香落ち", "角落ち", "飛車落ち", "飛香落ち",
+        "二枚落ち", "三枚落ち", "四枚落ち", "五枚落ち",
+        "六枚落ち", "八枚落ち", "十枚落ち",
+        "歩三兵", "裸玉"),
+      English -> Seq("Even", "Mating (Black)", "Mating (White)",
+        "Lance", "Bishop", "Rook", "Rook-Lance",
+        "2-Piece", "3-Piece", "4-Piece", "5-Piece",
+        "6-Piece", "8-Piece", "10-Piece",
+        "Three Pawns", "Naked King")
     )
 
     private[this] val buttons = states.map(st => button(cls := "btn btn-default col-sm-3 col-sm-offset-1", onclick := { () => Controller.setEditInitialState(st) }, "").render)
