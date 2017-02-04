77ad0b57337f84bc232b708b9104f115357025b4
Yosuke Mizutani <mogproj@gmail.com>
2017-02-04 12:14:15 -0500

    implemented tooltip

--- src/main/scala/com/mogproject/mogami/playground/view/modal/BootstrapJQuery.scala
+++ src/main/scala/com/mogproject/mogami/playground/view/modal/BootstrapJQuery.scala
@@ -4,9 +4,27 @@ import org.scalajs.jquery.JQuery
 
 import scala.scalajs.js
 
+// todo: move to another package
+
 @js.native
 trait BootstrapJQuery extends JQuery {
   def modal(action: String): BootstrapJQuery = js.native
 
   def modal(options: js.Any): BootstrapJQuery = js.native
+
+  def tooltip(options: js.Any = ???): BootstrapJQuery = js.native
+}
+
+@js.native
+trait TooltipOptions extends js.Object {
+  var animation: Boolean = js.native
+  var container: js.Any = js.native
+  var delay: js.Any = js.native
+  var html: Boolean = js.native
+  var placement: js.Any = js.native
+  var selector: js.Any = js.native
+  var template: String = js.native
+  var title: js.Any = js.native
+  var trigger: String = js.native
+  var viewport: js.Any = js.native
 }
