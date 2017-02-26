5d9f10733c3eb1deaf2eb573a3f3616cd00c417e
Yosuke Mizutani <mogproj@gmail.com>
2017-02-25 21:07:53 -0500

    added help messages

--- src/main/scala/com/mogproject/mogami/playground/view/section/EditSection.scala
+++ src/main/scala/com/mogproject/mogami/playground/view/section/EditSection.scala
@@ -17,7 +17,8 @@ object EditSection extends Section {
     EditReset.output,
     h4("Help"),
     ul(
-      li("Click on a player name to set the turn to move.")
+      li("Click on a player name to set the turn to move."),
+      li("Double-click on a piece on the board to change the piece attributes: Black Unpromoted -> Black Promoted -> White Unpromoted -> White Promoted -> Black Unpromoted.")
     )
   ).render
 }
