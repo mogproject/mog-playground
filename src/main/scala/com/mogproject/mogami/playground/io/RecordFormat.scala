eb152046b669de4d61640d6df254112d3a846ff6
Yosuke Mizutani <mogproj@gmail.com>
2017-04-02 00:20:05 -0400

    added a textbox area for record loading

---
+++ src/main/scala/com/mogproject/mogami/playground/io/RecordFormat.scala
@@ -0,0 +1,25 @@
+package com.mogproject.mogami.playground.io
+
+/**
+  *
+  */
+
+sealed trait RecordFormat
+
+case object CSA extends RecordFormat
+
+case object KIF extends RecordFormat
+
+case object KI2 extends RecordFormat
+
+object RecordFormat {
+  def guessFormat(s: String): RecordFormat = {
+    val ls = s.split("\n")
+    if (ls.exists(x => x.startsWith("▲") || x.startsWith("△")))
+      KI2
+    else if (ls.exists(x => x.startsWith("先手：")))
+      KIF
+    else
+      CSA
+  }
+}
--- src/main/scala/com/mogproject/mogami/playground/view/parts/RecordLoadButton.scala
+++ src/main/scala/com/mogproject/mogami/playground/view/parts/RecordLoadButton.scala
@@ -1,11 +1,12 @@
 package com.mogproject.mogami.playground.view.parts
 
 import com.mogproject.mogami.playground.controller.Controller
+import com.mogproject.mogami.playground.io.RecordFormat
 import com.mogproject.mogami.playground.view.EventManageable
 import com.mogproject.mogami.playground.view.bootstrap.Tooltip
 import com.mogproject.mogami.util.Implicits._
 import org.scalajs.dom
-import org.scalajs.dom.html.{Div, Input, Label}
+import org.scalajs.dom.html._
 import org.scalajs.dom.raw.FileReader
 
 import scala.util.Try
@@ -43,6 +44,37 @@ object RecordLoadButton extends EventManageable {
     data("placement") := "bottom"
   ).render
 
+  private[this] lazy val textBoxElem: TextArea = textarea(
+    cls := "form-control",
+    rows := 5,
+    placeholder := "Paste your record here.",
+    data("toggle") := "tooltip",
+    data("trigger") := "manual",
+    data("placement") := "top"
+  ).render
+
+  private[this] lazy val loadButton: Button = button(
+    tpe := "button",
+    cls := "btn btn-default btn-block",
+    data("toggle") := "tooltip",
+    data("placement") := "bottom",
+    data("dismiss") := "modal",
+    data("original-title") := s"Load a record from the text area",
+    onclick := { () =>
+      val text = textBoxElem.value
+      val format = RecordFormat.guessFormat(text)
+      displayMessageRecordLoadText(s"Loading as ${format} Format...")
+      loadButton.disabled = true
+      dom.window.setTimeout(() => readRecordText(format, text), 500)
+    },
+    "Load"
+  ).render
+
+  private[this] lazy val messageArea: Div = div(
+    cls := "col-md-9 col-xs-9 text-muted",
+    marginTop := 6
+  ).render
+
   lazy val output: Div = div(
     label("Load from File"),
     div(cls := "input-group",
@@ -51,6 +83,15 @@ object RecordLoadButton extends EventManageable {
         browseButton
       ),
       textElem
+    ),
+    br(),
+    label("Load from Text"),
+    textBoxElem,
+    br(),
+    div(
+      cls := "row",
+      div(cls := "col-md-3 col-xs-3", loadButton),
+      messageArea
     )
   ).render
 
@@ -58,9 +99,16 @@ object RecordLoadButton extends EventManageable {
     textElem.value = message
   }
 
-
   def displayTooltip(message: String): Unit = {
     Tooltip.display(textElem, message, 2000)
+  }
+
+  def displayMessageRecordLoadText(message: String): Unit = {
+    messageArea.innerHTML = message
+  }
+
+  def displayTooltipRecordLoadText(message: String): Unit = {
+    Tooltip.display(textBoxElem, message, 2000)
   }
 
   private[this] def readSingleFile(callback: (String, String) => Unit): Unit = {
@@ -90,6 +138,11 @@ object RecordLoadButton extends EventManageable {
     }
   }
 
+  private[this] def readRecordText(format: RecordFormat, text: String): Unit = {
+    Controller.loadRecordText(format, text)
+    clearLoadText()
+  }
+
   def abort(message: String): Unit = {
     displayMessage(message)
     displayTooltip("Failed!")
@@ -98,5 +151,9 @@ object RecordLoadButton extends EventManageable {
 
   def clear(): Unit = {
     browseButton.disabled = false
+  }
+
+  def clearLoadText(): Unit = {
+    loadButton.disabled = false
   }
 }
