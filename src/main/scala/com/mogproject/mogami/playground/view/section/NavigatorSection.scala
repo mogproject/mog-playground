065079aa14faf418fe556ad373fd48070382bcc2
Yosuke Mizutani <mogproj@gmail.com>
2017-04-03 17:18:01 -0400

    published

--- src/main/scala/com/mogproject/mogami/playground/view/section/NavigatorSection.scala
+++ src/main/scala/com/mogproject/mogami/playground/view/section/NavigatorSection.scala
@@ -11,23 +11,21 @@ import scalatags.JsDom.all._
   * Navbar
   */
 case class NavigatorSection(layout: Layout) extends Section {
-  override val output: Div = {
-    div(
-      div(cls := "container", padding := 0,
-        div(cls := "row")(
-          div(cls := "navbar-header col-md-10 col-md-offset-1", width := "100%",
-            ul(cls := "nav navbar-nav",
-              li(cls := "navbar-brand hidden-xs hidden-sm", "Shogi Playground"),
-              li(ModeSelector.output),
-              FlipButton.output,
-              li(cls := "pull-right visible-xs visible-sm", MenuButton.output)
-            )
+  override val output: Div = div(
+    div(cls := "container", padding := 0,
+      div(cls := "row")(
+        div(cls := "navbar-header col-md-10 col-md-offset-1", width := "100%",
+          ul(cls := "nav navbar-nav",
+            li(cls := "navbar-brand hidden-xs hidden-sm", "Shogi Playground"),
+            li(ModeSelector.output),
+            FlipButton.output,
+            li(cls := "pull-right visible-xs visible-sm", MenuButton.output)
           )
         )
       )
-    ).render
-  }
-
+    )
+  ).render
+  
   def updateMode(mode: Mode): Unit = {
     ModeSelector.updateValue(mode)
     updateBackground(mode)
