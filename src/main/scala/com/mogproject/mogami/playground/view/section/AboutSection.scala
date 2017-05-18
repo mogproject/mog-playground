45f48404c321cb7f386fbd99724103180f1d02ba
Yosuke Mizutani <mogproj@gmail.com>
2017-05-18 18:48:00 -0400

    updated about section

--- src/main/scala/com/mogproject/mogami/playground/view/section/AboutSection.scala
+++ src/main/scala/com/mogproject/mogami/playground/view/section/AboutSection.scala
@@ -18,6 +18,7 @@ object AboutSection extends Section {
       p(i(""""Run anywhere. Needs NO installation."""")),
       p("Shogi Playground is a platform for all shogi --Japanese chess-- fans in the world." +
         " This mobile-friendly website enables you to manage, analyze, and share shogi games as well as mate problems."),
+      p("This website best fits with ", a(href := "https://source.typekit.com/source-han-serif/", "Source Han Serif"), " font."),
       p("If you have any questions, trouble, or suggestion, please tell the ",
         a(href := "https://twitter.com/mogproject", target := "_blank", "author"),
         ". Your voice matters.")
